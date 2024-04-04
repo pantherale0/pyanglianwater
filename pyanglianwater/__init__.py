d435e42f9f5505818ea685b6a7496f6b7d10f969
Jordan <jordan@hrvy.uk>
2024-04-04 19:40:36 +0100

    initial commit

---
+++ pyanglianwater/__init__.py
@@ -0,0 +1,1 @@
+"""The core Anglian Water module."""
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
 
 # Inspiration: https://stackoverflow.com/a/7071358/6064135
-with open('project_name/_version.py', 'r') as version_file:
+with open('pyanglianwater/_version.py', 'r') as version_file:
     version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
     if version_groups:
         version = version_groups.group(1)
@@ -31,13 +31,13 @@ DEV_REQUIREMENTS = [
 ]
 
 setuptools.setup(
-    name='PROJECT_NAME_URL',
+    name='pyanglianwater_URL',
     version=version,
     description='Your project description here',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/USERNAME/PROJECT_NAME_URL',
-    author='USERNAME',
+    url='http://github.com/pantherale0/pyanglianwater',
+    author='pantherale0',
     license='MIT',
     packages=setuptools.find_packages(
         exclude=[
@@ -46,7 +46,7 @@ setuptools.setup(
         ]
     ),
     package_data={
-        'project_name': [
+        'pyanglianwater': [
             'py.typed',
         ]
     },
@@ -58,11 +58,6 @@ setuptools.setup(
     install_requires=REQUIREMENTS,
     extras_require={
         'dev': DEV_REQUIREMENTS,
-    },
-    entry_points={
-        'console_scripts': [
-            'PROJECT_NAME_URL=project_name.my_module:main',
-        ]
     },
     python_requires='>=3.8, <4',
 )
